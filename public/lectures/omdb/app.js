9fe9f85fce2185e507426d35436f6ad4c6f79974
Israel <213withani@gmail.com>
2017-11-01 22:59:27 -0700

    Make api call and display returned json

--- public/lectures/omdb/app.js
+++ public/lectures/omdb/app.js
@@ -1,9 +1,11 @@
 (function () {
+    // Usually service lets you do CRUD operations
     angular
         .module("omdbApp", [])
-        .controller("searchController", searchController);
+        .controller("searchController", searchController)
+        .service("movieService", movieService);
 
-    function searchController() {
+    function searchController(movieService) {
         var model = this;
         //Event handler
         model.searchMovieByTitle = searchMovieByTitle;
@@ -14,7 +16,29 @@
         init();
 
         function searchMovieByTitle(movieTitle) {
-            alert(movieTitle);
+            movieService
+                .searchMovieByTitle(movieTitle)
+                .then(renderMovies);
+        }
+
+        function renderMovies(movies) {
+            //instance controller = server info
+            model.movies = movies;
+        }
+    }
+
+    function movieService($http) {
+        this.searchMovieByTitle = searchMovieByTitle;
+
+        function searchMovieByTitle(movieTitle) {
+            url = "http://www.omdbapi.com/?s=" + movieTitle + "&apikey=852159f0";
+            // http injected into the service method
+            return $http.get(url)
+                .then(function (response) {
+                    //unwrap data
+                    return response.data;
+                })
+                ;
         }
     }
 })();
