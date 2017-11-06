bc28b762adda6c73699320c54788aa62bba0e76a
Israel <213withani@gmail.com>
2017-11-05 16:58:54 -0800

    refactored

--- public/lectures/omdb/app.js
+++ public/lectures/omdb/app.js
@@ -23,6 +23,7 @@
 
         function renderMovies(movies) {
             //instance controller = server info
+            // when you get the movies back, display them
             model.movies = movies;
         }
     }
@@ -33,6 +34,7 @@
         function searchMovieByTitle(movieTitle) {
             url = "http://www.omdbapi.com/?s=" + movieTitle + "&apikey=852159f0";
             // http injected into the service method
+            // Go to the url, then return the data retrieved.
             return $http.get(url)
                 .then(function (response) {
                     //unwrap data
