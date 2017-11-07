77ccb069ae52ffbc927fff2ab7d999ab40345b73
Israel <213withani@gmail.com>
2017-11-06 20:31:32 -0800

    On btn click we want to call the event handler and display the movies in console

--- public/lectures/jquery/omdb.js
+++ public/lectures/jquery/omdb.js
@@ -2,13 +2,26 @@
     $(init);
 
     function init() {
+        // Bind to the html tags
         var searchByTitleBtn = $("#searchByTitleBtn");
         var searchByTitleFld = $("#searchByTitleFld");
 
+        // Event handler
         searchByTitleBtn.click(searchByTitle);
 
         function searchByTitle() {
-            alert("searchByTitle");
+            var movieTitle = searchByTitleFld.val();
+            url="www.omdbapi.com/?s="+movieTitle+"&apikey=852159f0"
+            $.ajax({
+                url: url,
+                success: renderMovies,
+                error:function () {
+                    alert("oops");
+                } 
+            });
+        }
+        function renderMovies(response) {
+            console.log(response);
         }
     }
 })();
