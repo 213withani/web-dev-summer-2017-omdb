c27b23a443008dd0d9fbdb3b99976348f4028a56
Israel <213withani@gmail.com>
2017-11-07 21:20:08 -0800

    Displaying widgets declared in cotroller and using directive to display them

--- public/lectures/directives/app.js
+++ public/lectures/directives/app.js
@@ -5,13 +5,25 @@
         .directive("itemList", itemListDirective)
         .directive("hello", helloDirective);
 
+    function itemListDirective() {
+        return {
+            templateUrl: "widget-list.html"
+        }
+    }
+
     function helloDirective() {
         return {
             templateUrl: "helloWorld.html"
-        };
+        }
     }
 
     function directiveController($scope) {
         $scope.message = "Hello Controller";
+        $scope.widgets = [
+            { "type": "HEADING" },
+            { "type": "IMAGES" },
+            { "type": "YOUTUBE" },
+            { "type": "HTML" }
+        ];
     }
 })();
